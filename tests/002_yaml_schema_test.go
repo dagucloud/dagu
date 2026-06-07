d2d8ed286c37a212dc411b49ab4c6685e60f86a4
Yota Hamada <yohamta@gmail.com>
2026-06-07 23:57:56 +0900

    test: add v3 binary conformance test

---
+++ tests/002_yaml_schema_test.go
@@ -0,0 +1,23 @@
+package tests_test
+
+import (
+	"testing"
+
+	dagutest "github.com/dagucloud/dagu/tests/internal"
+)
+
+func Test002YAMLSchema(t *testing.T) {
+	t.Parallel()
+
+	t.Run("entrypoint name is forbidden", func(t *testing.T) {
+		t.Parallel()
+
+		dagu := dagutest.New(t, "002_yaml_schema")
+
+		result := dagu.Run("workflow", "validate", ".dagu/entrypoint_name_forbidden.yaml")
+		result.ExpectExitCode(1)
+		result.ExpectStdout("")
+		result.ExpectStderrContains("entrypoint", "name")
+		dagu.ExpectNoFile("executed.txt")
+	})
+}
